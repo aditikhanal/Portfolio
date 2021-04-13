9fb86ecf468b2694e7e36051554f97248efff08f
aditikhanal <its.aditikhanal@gmail.com>
2021-04-13 18:26:01 +0545

    hello

--- server.js
+++ server.js
@@ -1,2 +1,6 @@
-var express = require('express');
-var app = express();app.use(express.static(__dirname + '/'));app.listen(process.env.PORT || 8080);
+
+var connect = require("connect");
+
+var app = connect().use(connect.static(__dirname + '/src'));
+
+app.listen(8180);
