eb04bf77654dcac7531a9c09e9aa50fb5fef77cc
aditikhanal <its.aditikhanal@gmail.com>
2021-04-13 16:34:58 +0545

    added app directory

--- server.js
+++ server.js
@@ -1,2 +1,2 @@
 var express = require('express');
-var app = express();app.use(express.static("app/src" + '/'));app.listen(process.env.PORT || 8080);
+var app = express();app.use(express.static("portfolio/src" + '/'));app.listen(process.env.PORT || 8080);
